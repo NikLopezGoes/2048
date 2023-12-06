9907c43386490aca31cce6baf6974b99113b5dac
NikLopezGoes <119625116+NikLopezGoes@users.noreply.github.com>
2023-12-05 19:41:13 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import game_internals
 import structs
 import random
 
+legalMove = True
 gameOver = False
 gameBoard = structs.state()
 number = random.randint(0,9)
@@ -12,6 +13,7 @@ else:
     game_internals.add_number(gameBoard.board,2)
 
 while(gameOver == False):
+    legalMove = True
     print("please input a move W, A, S, D")
     print(gameBoard)
     direction = input()
@@ -27,13 +29,18 @@ while(gameOver == False):
     elif(direction == 's'):
         (grid,changed) = game_internals.move_down(gameBoard.board)
         gameBoard.board = grid
-    if(game_internals.isGameOver(gameBoard.board) == True):
-        gameOver = True
-        break
     else:
-        number = random.randint(0,9)
-        if(number == 4):
-            game_internals.add_number(gameBoard.board,4)
+        print("not a legal move")
+        legalMove = False
+    
+    if(legalMove == True):
+        if(game_internals.isGameOver(gameBoard.board) == True):
+            gameOver = True
+            break
         else:
-            game_internals.add_number(gameBoard.board,2)
+            number = random.randint(0,9)
+            if(number == 4):
+                game_internals.add_number(gameBoard.board,4)
+            else:
+                game_internals.add_number(gameBoard.board,2)
 
